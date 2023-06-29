e15251d31582df699d63d80acd0f6385b9408e3c
Niloy <106124483+mniloy1123@users.noreply.github.com>
2023-06-29 17:05:15 -0400

    Setup route to serve up all students

---
+++ api/students.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const router = express.Router();
+const { Students } = require("../db/models");
+
+//Root here is localhost:8080/api/students/
+router.get("/", async (req, res, next) => {
+  try {
+    const allStudents = await Students.findAll();
+
+    allStudents
+      ? res.status(200).json(allStudents)
+      : res.status(404).send("Students Listing Not Found");
+  } catch (error) {
+    next(error);
+  }
+});
+
+module.exports = router;
