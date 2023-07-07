a27835cb58dec6507360517c3ad63d06cd3d9919
Niloy <106124483+mniloy1123@users.noreply.github.com>
2023-07-07 12:58:49 -0400

    Configs

--- index.js
+++ index.js
@@ -3,19 +3,10 @@ const express = require("express");
 const db = require("./db");
 const app = express();
 const PORT = "8080";
-const cors = require('cors');
 
-//app.use(cors());
-
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*"); // or your frontend origin instead of "*"
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
 
 //Parse JSON bodies
 app.use(express.json());
-
 
 //Mount on API
 app.use("/api", require("./api"));
@@ -32,3 +23,4 @@ syncDB();
 serverRun();
 
 module.exports = app;
+
