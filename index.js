02ba3c92f19f0add537b44cba9d634dea151a24f
Niloy <106124483+mniloy1123@users.noreply.github.com>
2023-07-07 12:51:59 -0400

    Configs

--- index.js
+++ index.js
@@ -5,7 +5,13 @@ const app = express();
 const PORT = "8080";
 const cors = require('cors');
 
-app.use(cors());
+//app.use(cors());
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*"); // or your frontend origin instead of "*"
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 //Parse JSON bodies
 app.use(express.json());
