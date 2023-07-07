b3584c5cb43e347ac1e84c30623b3c8c92c7080a
Niloy <106124483+mniloy1123@users.noreply.github.com>
2023-07-07 13:15:56 -0400

    Require pg manually

--- db/db.js
+++ db/db.js
@@ -1,7 +1,8 @@
 const { Sequelize } = require("sequelize");
 
 const db = new Sequelize(process.env.DATABASE_URL, {
-    logging: false,
+  logging: false,
+  dialectModule: require("pg"),
 });
 
 module.exports = db;
